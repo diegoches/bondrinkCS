5ae73bf54f80949eec73c889aa461ca735be54d0
Diego Chávez Escalante <ddce.2005@gmail.com>
2017-07-26 21:39:35 -0500

    Adding word2vec class first structure.

---
+++ pre_processors/word2vec.py
@@ -0,0 +1,13 @@
+class Word2Vec(object):
+
+    def __init__(self):
+        pass
+
+    def add_word(self, word):
+        pass
+
+    def train_net(self):
+        pass
+
+    def extract_vector(self, word):
+        pass
